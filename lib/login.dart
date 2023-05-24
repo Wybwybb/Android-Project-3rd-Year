ffa5eb794de57e80f11fd19e23339947bf916e57
Wybwybb <rybrybb@gmail.com>
2023-05-24 10:37:54 +0800

    UPdated

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,3 @@
-import 'package:android_exam/main.dart';
 import 'package:flutter/material.dart';
 
 import 'mainpage.dart';
@@ -12,50 +11,62 @@ class LoginPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Branzuela, Raymond Benedict'),
       ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Enter your ID Number', // Added text above the text field
-                style: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromARGB(255, 0, 134, 244),
+              Color.fromARGB(255, 254, 237, 0)
+            ], // Customize the gradient colors here
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Enter your ID Number', // Added text above the text field
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              SizedBox(height: 10.0),
-              TextField(
-                controller: idController,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: 'Student ID',
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.person),
+                SizedBox(height: 10.0),
+                TextField(
+                  controller: idController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Student ID',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.person),
+                  ),
                 ),
-              ),
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                onPressed: () {
-                  final String studentID = idController.text;
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => MainPage(studentID: studentID),
-                    ),
-                  );
-                },
-                child: Text(
-                  'Login',
-                  style: TextStyle(fontSize: 18.0),
+                SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {
+                    final String studentID = idController.text;
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => MainPage(studentID: studentID),
+                      ),
+                    );
+                  },
+                  child: Text(
+                    'Login',
+                    style: TextStyle(fontSize: 18.0),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 40.0, vertical: 16.0),
+                  ),
                 ),
-                style: ElevatedButton.styleFrom(
-                  padding:
-                      EdgeInsets.symmetric(horizontal: 40.0, vertical: 16.0),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
